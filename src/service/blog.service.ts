5623634aaddfaa0141c3c76751a50c6e6ea93989
Doniyorov Doniyor <ddonierov68@gmail.com>
2023-05-23 11:31:20 +0500

    deploy

--- src/service/blog.service.ts
+++ src/service/blog.service.ts
@@ -5,23 +5,23 @@ import { BlogType } from "src/interfaces/blog.interfaces";
 
 export const BlogService = {
     async getAll() {
-        const { data } = await axios.get<BlogType[]>(`http://localhost:8080/api/blog`)
+        const { data } = await axios.get<BlogType[]>(`https://nestjs-backend.onrender.com/api/blog`)
         return data
     },
     async addBlog(dto: object) {
-        const { data } = await axios.post<object>(`http://localhost:8080/api/blog`, dto)
+        const { data } = await axios.post<object>(`https://nestjs-backend.onrender.com/api/blog`, dto)
         return data
     },
     async getBySlug(slug: string) {
-        const { data } = await axios.get<BlogType>(`http://localhost:8080/api/blog/${slug}`)
+        const { data } = await axios.get<BlogType>(`https://nestjs-backend.onrender.com/api/blog/${slug}`)
         return data
     },
     async deleteBlog(slug: string) {
-        const { data } = await axios.delete(`http://localhost:8080/api/blog/${slug}`)
+        const { data } = await axios.delete(`https://nestjs-backend.onrender.com/api/blog/${slug}`)
         return data
     },
     async editBlog(slug: string, blog: object) {
-        const { data } = await axios.put(`http://localhost:8080/api/blog/${slug}`, blog)
+        const { data } = await axios.put(`https://nestjs-backend.onrender.com/api/blog/${slug}`, blog)
         return data
     }
 }
